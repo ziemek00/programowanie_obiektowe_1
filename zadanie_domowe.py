64dc11336a0aa210a6da2e814dbd359f250cdc90
ziemek00 <ziomag7@gmail.com>
2024-03-07 11:46:41 +0100

    Initial Commit

--- zadanie_domowe.py
+++ zadanie_domowe.py
@@ -1,6 +1,3 @@
 # Napisz program który umożliwi mnożenie dwóch liczb całkowitych x i y podawanych z klawiatury.
 # Program powinien wyświetlać wynik na ekranie. Dla wyjścia z programu użytkownik powninien wcisnąć "e"
 
-def mnozenie():
-    while True:
-        try:
