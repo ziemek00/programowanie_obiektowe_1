e724c172c5d0e1bd68306d55b8ddc8d728d40317
ziemek00 <ziomag7@gmail.com>
2024-03-07 11:55:15 +0100

    Commit

--- zadanie_domowe.py
+++ zadanie_domowe.py
@@ -1,26 +1,32 @@
 # Napisz program który umożliwi mnożenie dwóch liczb całkowitych x i y podawanych z klawiatury.
 # Program powinien wyświetlać wynik na ekranie. Dla wyjścia z programu użytkownik powninien wcisnąć "e"
 
+import sys
 def mnozenie():
-
     while True:
-        x = input("Podaj pierwszą liczbę całkowitą. Jeśli naciśniesz 'e' program skończy pracę.")
-        if x.lower() == 'e':
-            print('Program skończył pracę.')
-            break
-        try:
-            x=int(x)
-            break
-        except ValueError:
-            print('Podano złą wartość. Spróbuj jeszcze raz wprowadzić liczbę całkowitą.')
+        while True:
+            x = input("Podaj pierwszą liczbę całkowitą. Jeśli naciśniesz 'e' program skończy pracę.")
+            if x.lower() == 'e':
+                print('Program skończył pracę.')
+                sys.exit()
+            try:
+                x=int(x)
+                break
+            except ValueError:
+                print('Podano złą wartość. Spróbuj jeszcze raz wprowadzić liczbę całkowitą.')
 
-    while True:
-        x = input("Podaj pierwszą liczbę całkowitą. Jeśli naciśniesz 'e' program skończy pracę.")
-        if x.lower() == 'e':
-            print('Program skończył pracę.')
-            break
-        try:
-            x=int(x)
-            break
-        except ValueError:
-            print('Podano złą wartość. Spróbuj jeszcze raz wprowadzić liczbę całkowitą.')
+        while True:
+            y = input("Podaj pierwszą drugą całkowitą. Jeśli naciśniesz 'e' program skończy pracę.")
+            if y.lower() == 'e':
+                print('Program skończył pracę.')
+                sys.exit()
+            try:
+                y=int(y)
+                break
+            except ValueError:
+                print('Podano złą wartość. Spróbuj jeszcze raz wprowadzić liczbę całkowitą.')
+
+        wynik = x * y
+        print(f"Wynik mnożenia liczby '{x}' i liczby '{y}' to: '{wynik}' ")
+
+mnozenie()
