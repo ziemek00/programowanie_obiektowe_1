59edde2ed766995c6528739c9c59be1b8ae53470
ziemek00 <ziomag7@gmail.com>
2024-03-07 11:49:23 +0100

    Commit

--- zadanie_domowe.py
+++ zadanie_domowe.py
@@ -1,3 +1,26 @@
 # Napisz program który umożliwi mnożenie dwóch liczb całkowitych x i y podawanych z klawiatury.
 # Program powinien wyświetlać wynik na ekranie. Dla wyjścia z programu użytkownik powninien wcisnąć "e"
 
+def mnozenie():
+
+    while True:
+        x = input("Podaj pierwszą liczbę całkowitą. Jeśli naciśniesz 'e' program skończy pracę.")
+        if x.lower() == 'e':
+            print('Program skończył pracę.')
+            break
+        try:
+            x=int(x)
+            break
+        except ValueError:
+            print('Podano złą wartość. Spróbuj jeszcze raz wprowadzić liczbę całkowitą.')
+
+    while True:
+        x = input("Podaj pierwszą liczbę całkowitą. Jeśli naciśniesz 'e' program skończy pracę.")
+        if x.lower() == 'e':
+            print('Program skończył pracę.')
+            break
+        try:
+            x=int(x)
+            break
+        except ValueError:
+            print('Podano złą wartość. Spróbuj jeszcze raz wprowadzić liczbę całkowitą.')
