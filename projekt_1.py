42977b4426f1cbb39321fdafc84990380f969b2d
ziemek00 <ziomag7@gmail.com>
2024-03-19 23:33:30 +0100

    Commit

--- projekt_1.py
+++ projekt_1.py
@@ -4,29 +4,50 @@
 # zdobytych punktow. poziomy powinny byc wyswietlane jako liczby rzymskie(napisac
 # funkcje, ktora bedzie przetwarzala liczbe w liczbe rzymska). 1 tyg od teraz (czyli na
 # 21 marca mam przyniesc).
-class Clicker:
-    def __init__(self, points, level, press):
-        self.points = points
-        self.level = level
-        self.press = press
 
 class __Model:
+    def __init__(self):
+        self.points = 0
+        self.level = 1
     def add_points(self):
-        self.points = []
-        for point in self.points:
-            pass
+        self.points += 1
+        self.add_level()
+        print(self.points)
     def add_level(self):
-        self.level = []
-        pass
+        self.level = self.points // 5 + 1
 
 class View:
+    def __init__(self, model):
+        self.model = model
     def show_instructions(self):
-        pass
-    def rome_letters(self):
-        pass
+        print("To jest clicker. Naciskaj 'b' aby zdobywać puntky. Naciśnij 'e' aby zakończyć działanie programu.")
     def show_points_and_level(self):
         pass
+    def show_end(self):
+        print(f'Zakończyłeś grę z wynikiem {self.model.points} i {self.model.level} poziomem.')
+        quit()
 
 class __Controller:
+    def __init__(self, view, model):
+        self.model = model
+        self.view = view
     def press_button(self):
-        pass
+        self.points = []
+        while True:
+            click = input()
+            if click.lower() == 'e':
+                self.view.show_end()
+            if click.lower() == 'b':
+                self.model.add_points()
+            else:
+                print("Musisz klikac w 'b' aby zdobyć punkt, lub w 'e' aby zakończyć grę")
+
+def main():
+    model = __Model()
+    view = View(model)
+    controller = __Controller(view, model)
+    view.show_instructions()
+    controller.press_button()
+
+if __name__ == "__main__":
+    main()
